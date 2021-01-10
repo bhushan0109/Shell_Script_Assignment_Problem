7f150ce942f704656a21418fa6a9bc7cead6c895
Bhushan Patil <75246941+bhushan0109@users.noreply.github.com>
2021-01-10 13:17:12 +0530

    Update RandomSumAvg.sh

--- sequence_prob/RandomSumAvg.sh
+++ sequence_prob/RandomSumAvg.sh
@@ -1,18 +1,18 @@
 #! /bin/bash -x
-#write the program that read 5 random with 2 digit values,then find there sum and the average
+#write the program that read 5 random with 2 digit values,then find there sum a>
 #SOLVE:
 #! /bin/bash
 sum=0
 
-for a in $(seq 5 );
+for ((i=1;i<=5;i++ ))
 do
 
 x=${RANDOM:0:2}
 echo $x;
 sum=$((sum+x))
 
- done
+done
 echo "sum is $sum "
- 
+
 avg=`echo $sum 5 |awk '{ print $1/$2 }'`
 echo "avg is $avg "
