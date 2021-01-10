7cc16afd6f2564ff33c81aaa739254f4f2b1d98a
Bhushan Patil <75246941+bhushan0109@users.noreply.github.com>
2021-01-10 13:50:19 +0530

    Update TwoDiceAdd.sh

--- sequence_prob/TwoDiceAdd.sh
+++ sequence_prob/TwoDiceAdd.sh
@@ -4,10 +4,9 @@
 #add two random dice number and print the result
 #solve:
 
-dice1=$RANDOM; ((dice1 = dice1 % 6 +1 ));
-dice2=$RANDOM; ((dice2 = dice2 % 6 +1 ));
+dice1=$(( $RANDOM%6 + 1 ))
+dice2=$(( $RANDOM%6 +1 ))
 pair="$dice1 - $dice2"
 echo  $pair
 Add=$(($dice1 + $dice2))
 echo  $Add
-
