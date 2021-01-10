003ec37874e0101ff02305d557ac10d8fd69f342
Bhushan Patil <75246941+bhushan0109@users.noreply.github.com>
2021-01-10 16:11:09 +0530

    Update LeapYear.sh

--- If_Else_prob/LeapYear.sh
+++ If_Else_prob/LeapYear.sh
@@ -1,24 +1,17 @@
 #! /bin/bash -x
-#QUE.3 leap year finding
+#QUE.3 
+#!/bin/bash -x
+
+echo "Enter year to check leap year or not "
+read -p " " year
 
 
-  echo "Enter the year (YYYY)" year
-read year
+        if  [ $(($year % 4)) -eq 0 ] && [ $(($year % 100)) -ne 0 ] || [ $(($year % 400)) -eq 0 ]
+        then
+                echo " " $year "is leap year"
+        else
+                echo $year "is not leap year"
+        fi
 
-if [ $((year % 4)) -eq 0 ]
-then
-  if [ $((year % 100)) -eq 0 ]
-    then
-    if [ $((year % 400)) -eq 0 ]
-          then
-        echo "its a leap year"
-    else
-           echo "its not a leap year"
-    fi
-  else
-  echo "Its a leap year"
-  fi
-else
- echo "its not a leap year"
 
-fi
+
