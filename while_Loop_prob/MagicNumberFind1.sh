4ae38faa9f2e1e637d15c1a4f4c7df188737aafe
Bhushan Patil <75246941+bhushan0109@users.noreply.github.com>
2021-01-10 20:09:06 +0530

    Update MagicNumberFind1.sh

--- while_Loop_prob/MagicNumberFind1.sh
+++ while_Loop_prob/MagicNumberFind1.sh
@@ -1,40 +1,42 @@
+
+#! /bin/bash -x
 #!/bin/bash -x
 #find magic number which user enter and check by user with comparing
 
 echo "Please think of a number between 0 and 100! "
- read user_num
+ #read user_num
 li=1
 hi=100
 mi=$((($li + $hi) / 2 ))
 
 
-while [ $li -le $hi ]
+while ((1))
 do
-    echo "please compare  with $mi and choose higher for 'h' or lower for 'l' "
+    echo "please compare :   $mi is  greater type 'h' or $mi is lower type 'l' "
 
     read  choice
 case $choice in
-	h)
+        h)
 
-	hi=$(($mi - 1))
-			;;
-	l) 
+        hi=$(($mi  ))
+                        ;;
+        l)
 
-	li=$(($mi + 1))
-	;;
-	y)
-	echo "match"
-	exit
-	;;
- 	*)
- 	echo “wrong entry”
- 	;;
+        li=$(($mi ))
+        ;;
+        *)
+        echo “wrong entry”
+        ;;
 
  esac
  mi=$((($li + $hi) / 2 ))
-	if [ $user_num -eq $mi ]
-	then
-		echo " match = your num is $mi"
-		exit
-		fi
+        if [ $li == $mi ]
+        then
+                 if [ $hi -ge $li ]
+        	then
+                echo " match = your num is $mi"
+                exit
+                fi
+
+        fi
 done
