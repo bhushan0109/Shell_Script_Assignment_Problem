df74a45520719817e860713394a320f691b802da
Bhushan Patil <75246941+bhushan0109@users.noreply.github.com>
2021-01-10 22:01:28 +0530

    Update FlipCoin11time.sh

--- while_Loop_prob/FlipCoin11time.sh
+++ while_Loop_prob/FlipCoin11time.sh
@@ -1,34 +1,33 @@
 #!/bin/bash -x
 
-#!/bin/bash -x
-
 # VARIABLE
 head=1
 headCounter=0
 tailCounter=0
 
 # TO COUNT OF HEADS AND TAILS
-while [ $headCounter -le 10 -a $tailCounter -le 10 ]
+while [[ $headCounter -le 10  || $tailCounter -le 10 ]]
 do
-	flipCheck=$(( RANDOM % 2 ))
-	case $flipCheck in
-		$head)
-			((headCounter++))
-			;;
-		*)
-			((tailCounter++))
-			;;
-	esac
+        flipCheck=$(( RANDOM % 2 ))
+        case $flipCheck in
+                $head)
+                        ((headCounter++))
+                        ;;
+                *)
+                        ((tailCounter++))
+                        ;;
+        esac
 done
 
 # TO CHECK FLIP COIN WINNER
 if [ $headCounter -eq 11 ]
 then
-	echo Head Wins
+        echo Head Wins
 elif [ $tailCounter -eq 11 ]
 then
-	echo Tail Wins
+        echo Tail Wins
 else
-	echo Tie
+        echo Tie
 fi
+
 
